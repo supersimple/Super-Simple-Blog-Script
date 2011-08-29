4eed09dfdf9a7bd212af882668d3f56aa7c03664
Todd Resudek <toddresudek@gmail.com>
2011-08-29 17:07:07 -0600

    extending the XML parser functionality

--- class.xml-parser.php
+++ class.xml-parser.php
@@ -1,5 +1,5 @@
 <?php
-
+ini_set('display_errors','on');
 /***************************************************************************
 
 	Class XMLParser
@@ -20,6 +20,7 @@ Class XMLParser
 	
 	public $xmlfile;
 	public $xmlstr;
+	public $xmlarray;
 	
 	function __construct($xmlfile)
 	{
@@ -38,22 +39,101 @@ Class XMLParser
 	//method to read XML file and store as a string
 	public function parseXMLToString()
 	{
-		$fh = fopen($this->xmlfile, "r");
-		$xmlstr = fread($fh,filesize($this->xmlfile));
-		fclose($fh);
+		$xmlstr = simplexml_load_file($this->xmlfile);
 		return $xmlstr;
 	}
+
+	//method to get all posts
+	public function getAllPosts()
+	{
+		$posts = array();
+		
+		foreach($this->xmlstr->post as $post){
+			$this_guid = (string)$post->guid;
+			$posts[$this_guid] = array();
+			
+			$posts[$this_guid]['guid'] = (string)$post->guid;
+			$posts[$this_guid]['timestamp'] = (int)$post->timestamp;
+			$posts[$this_guid]['posttitle'] = (string)$post->posttitle;
+			$posts[$this_guid]['posttext'] = (string)$post->posttext;
+			$posts[$this_guid]['postedby'] = (int)$post->postedby;
+			$posts[$this_guid]['active'] = (int)$post->active;
+			
+			//merge the comments and images for each post
+			$posts[$this_guid]['images'] = $this->getAllImagesByGuid($this_guid);
+			$posts[$this_guid]['comments'] = $this->getAllCommentsByGuid($this_guid);
+			
+		}
+		
+		return $posts;
+	}		
+		
+	public function getAllImagesByGuid($guid){
+		//get an array of blog post images
+		$images = array();
+		
+		foreach($this->xmlstr->post as $post){
+			$this_guid = (string)$post->guid;
+			if($this_guid === $guid){
+					foreach($post->postimage as $postimage){
+						$images[] = array(
+							'imageid'=>(int)$postimage['imageid'],
+							'url'=>(string)$postimage
+						);
+					}
+			}
+		}
+		
+		return $images;
+	}
+	
+	
+	public function getAllCommentsByGuid($guid){
+		//get an array of blog post comments
+		$comments = array();
+		
+		foreach($this->xmlstr->post as $post){
+			$this_guid = (string)$post->guid;
+			if($this_guid === $guid){
+					foreach($post->comment as $comment){
+						$comments[] = array(
+							'timestamp'=>(string)$comment['timestamp'],
+							'url'=>(string)$comment['url'],
+							'name'=>(string)$comment['name'],
+							'text'=>(string)$comment
+						);
+					}
+			}
+		}
+		
+		return $comments;
+	}
+	
 	
 }
 
 
-$xmlobj = new XMLParser("settings.xml");
 
+//Just for testing////////////////////////////////////////////////////
+$xmlobj = new XMLParser("posts.xml");
+#print_r($xmlobj->xmlarray);
+
+$posts = $xmlobj->getAllPosts();
+print_r($posts);
+
+#$comments = $xmlobj->getAllComments();
+#print_r($comments);
+/*
 $xml = new SimpleXMLElement($xmlobj->xmlstr);
+print_r($xml);
 foreach($xml as $node){
 	foreach($node[0]->attributes() as $n=>$a){
 		echo "$n:$a";
 	}
 }
 
+foreach($xml as $nodek=>$nodev){
+		echo $nodek.':'.$nodev."\n";
+	}
+*/
 ?>

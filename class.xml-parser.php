3772e2da0671ef02d6b14048eb9ecd71a4c7d00a
Todd Resudek <toddresudek@gmail.com>
2011-08-30 13:39:18 -0600

    ADding functionality to teh XML parser

--- class.xml-parser.php
+++ class.xml-parser.php
@@ -68,14 +68,18 @@ Class XMLParser
 		return $posts;
 	}		
 		
-	public function getAllImagesByGuid($guid){
+	private function getAllImagesByGuid($guid)
+	{
 		//get an array of blog post images
 		$images = array();
 		
-		foreach($this->xmlstr->post as $post){
+		foreach($this->xmlstr->post as $post)
+		{
 			$this_guid = (string)$post->guid;
-			if($this_guid === $guid){
-					foreach($post->postimage as $postimage){
+			if($this_guid === $guid)
+			{
+					foreach($post->postimage as $postimage)
+					{
 						$images[] = array(
 							'imageid'=>(int)$postimage['imageid'],
 							'url'=>(string)$postimage
@@ -88,16 +92,20 @@ Class XMLParser
 	}
 	
 	
-	public function getAllCommentsByGuid($guid){
+	private function getAllCommentsByGuid($guid)
+	{
 		//get an array of blog post comments
 		$comments = array();
 		
-		foreach($this->xmlstr->post as $post){
+		foreach($this->xmlstr->post as $post)
+		{
 			$this_guid = (string)$post->guid;
-			if($this_guid === $guid){
-					foreach($post->comment as $comment){
+			if($this_guid === $guid)
+			{
+					foreach($post->comment as $comment)
+					{
 						$comments[] = array(
-							'timestamp'=>(string)$comment['timestamp'],
+							'timestamp'=>(int)$comment['timestamp'],
 							'url'=>(string)$comment['url'],
 							'name'=>(string)$comment['name'],
 							'text'=>(string)$comment
@@ -110,30 +118,155 @@ Class XMLParser
 	}
 	
 	
+	//method to get all settings
+	public function getAllSettings()
+	{
+		$settings = array();
+		
+			$setting = $this->xmlstr;
+			$settings['basepath'] = (string)$setting->basepath;
+			$settings['baseurl'] = (string)$setting->baseurl;
+			$settings['pagetitle'] = (string)$setting->pagetitle;
+			$settings['postsperpage'] = (string)$setting->postsperpage;
+			$settings['blogpagefilename'] = (string)$setting->blogpagefilename;
+			$settings['allowcomments'] = (string)$setting->allowcomments;
+			$settings['timeoffsetfromserver'] = (string)$setting->timeoffsetfromserver;
+			$settings['rssdescription'] = (string)$setting->rssdescription;
+			
+			//merge the users
+			$settings['users'] = $this->getAllUsers();
+		
+		return $settings;
+	}
+	
+	
+	//method to update settings
+	public function updateSettings($settings)
+	{
+		/*
+		<settings>
+	<basepath>str</basepath>
+	<baseurl>str</baseurl>
+	<pagetitle>str</pagetitle>
+	<postsperpage>int</postsperpage>
+	<blogpagefilename>str</blogpagefilename>
+	<user id="int" password="SHA1">str</user>
+	<allowcomments>bool</allowcomments>
+	<timeoffsetfromserver>int</timeoffsetfromserver>
+	<rssdescription>str</rssdescription>
+</settings>
+*/
+		//this method takes an array,containing 1 or more of the settings nodes to be updated
+		$setting = $this->xmlstr;
+		
+		foreach($settings as $node=>$value)
+		{
+				
+		}
+		
+	}
+	
+	private function getAllUsers()
+	{
+		
+		//get an array of users
+		$users = array();
+		
+		$setting = $this->xmlstr;
+					foreach($setting->user as $user)
+					{
+						$users[] = array(
+							'id'=>(int)$user['id'],
+							'password'=>(string)$user['password'],
+							'username'=>(string)$user
+						);
+					}
+
+		return $users;
+		
+	}
+	
+	private function getUser($uid)
+	{
+	
+	}
+	
+	private function addNewUser()
+	{
+	
+	}
+	
+	private function deleteUser($uid)
+	{
+	
+	}
+	
+	//method to add a new post to the posts XML
+	public function addNewPost($post)
+	{
+		$xml = $this->xmlstr;
+
+		$newpost = $xml->addChild('post');
+		$newpost->addChild('guid', md5(serialize($post)));
+		$newpost->addChild('timestamp', time());
+		$newpost->addChild('posttitle', '<![CDATA['.$post['posttitle'].']]>');
+		$newpost->addChild('posttext', '<![CDATA['.$post['posttext'].']]>');
+		$newpost->addChild('postedby', $post['postedby']);
+		$newpost->addChild('active', $post['active']);
+		
+		$i=1;
+		foreach($post['postimage'] as $postimage){
+			$newpostimage = $newpost->addChild('postimage', $postimage);
+			$newpostimage->addAttribute('imageid', $i);
+			$i++;
+		}
+		
+		//write the file
+		$xml->asXML($this->xmlfile);
+	}
+	
+	public function deletePost($guid)
+	{
+	
+	}
+	
+	public function editPost($guid,$post)
+	{
+	
+	}
+	
+	//method to add a comment to the posts XML
+	public function addNewComment($guid,$comment)
+	{
+		$xml = $this->xmlstr;
+		
+		//look for the post with this guid
+		foreach($xml->post as $post)
+		{
+			if($post->guid == $guid)
+			{
+				$newcomment = $post->addChild('comment',$comment['text']);
+				$newcomment->addAttribute('timestamp',time());
+				$newcomment->addAttribute('url',$comment['url']);
+				$newcomment->addAttribute('name',$comment['name']);
+			}
+		}
+		
+		//write the file
+		$xml->asXML($this->xmlfile);
+	}
+		
 }
 
 
 
 //Just for testing////////////////////////////////////////////////////
 $xmlobj = new XMLParser("posts.xml");
-#print_r($xmlobj->xmlarray);
+
+#$xmlobj->addNewPost(array('posttitle'=>htmlentities('Hey, man!'), 'posttext'=>htmlentities('This is so cool & stuff.\r\nAre you serious? "Yes, I am."'), 'postedby'=>1, 'active'=>1, 'postimage' => array('/file/path/images/imagename.jpg')));
+$xmlobj->addNewComment('XYZ7890',array('text'=>htmlentities('I think you need to check out http://cnn.com'),'url'=>htmlentities('http://twitter.com&user=heavy_t'),'name'=>htmlentities('Todd Resudek')));
 
 $posts = $xmlobj->getAllPosts();
 print_r($posts);
 
-#$comments = $xmlobj->getAllComments();
-#print_r($comments);
-/*
-$xml = new SimpleXMLElement($xmlobj->xmlstr);
-print_r($xml);
-foreach($xml as $node){
-	foreach($node[0]->attributes() as $n=>$a){
-		echo "$n:$a";
-	}
-}
-
-foreach($xml as $nodek=>$nodev){
-		echo $nodek.':'.$nodev."\n";
-	}
-*/
 ?>
